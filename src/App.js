d6f8a94586977dfd044f7615ac17a4a7f84cb883
Rajal Kumar <rajalkumar13@hotmail.com>
2019-02-14 12:38:19 +1300

    api added and working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
 
 import Title from './components/Title'
 // import MovieList from './components/MovieList'
 
 class App extends Component {
   render() {
+    console.log('API key', process.env.REACT_APP_TMDB_API_KEY)
     return (
       <div className="App">
         <Title title="Movie Picker"/>
